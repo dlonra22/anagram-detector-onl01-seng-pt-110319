71104288991e211a4aa87a5f4ccdd10e1e8789cc
arnie b <arnold.bwaila@gmail.com>
2020-01-05 17:18:12 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -17,15 +17,21 @@ def match(words)
     if sorted_words.length == sorted_word.length
       i = 0
       sorted_words.each do |chr|
-        if sorted_word[i] != chr {matched = false}
-         i++
+        if sorted_word[i] != chr 
+          matched = false
+        end
+        i++
       end # end of sorted_words.each
-    else matched? = false
+    else matched = false
     end #end of if sorted_words
-    if matched? == true { matches << wrd }
+    if matched == true
+      matches << wrd
+    end
   end #end of words.each
   matches
 end #end of method match
+
+end #end of  class
 
     
   
